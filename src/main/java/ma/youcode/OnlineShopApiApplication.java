2a78977714970bc276390d6a98b97cbfba2c2297
khouilid <abdelkbirkhouilid32@gmail.com>
2021-04-22 12:38:44 +0000

    add confirmation functionality

--- src/main/java/ma/youcode/OnlineShopApiApplication.java
+++ src/main/java/ma/youcode/OnlineShopApiApplication.java
@@ -2,9 +2,10 @@ package ma.youcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class OnlineShopApiApplication {
 
     public static void main(String[] args) {
